4fa187dff87f98021f40940b576627c76cf30dac
Javier Marte <kaison54@hotmail.com>
2016-08-24 07:50:19 -0400

    Made a couple of changes

--- app/src/main/java/com/example/kaiso/waterlogger/MainActivity.java
+++ app/src/main/java/com/example/kaiso/waterlogger/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kaiso.waterlogger;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+//        progressBar.setVisibility(View.VISIBLE);
+//        progressBar.setProgress(0);
+
+        final ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+       // final TextView progressText = (TextView) findViewById(R.id.progressText_detail);
+        //final ImageView btnCancel = (ImageView) findViewById(R.id.imgCancel_detail);
+        bar.post(new Runnable() {
+            @Override
+            public void run() {
+                bar.setProgress(10);
+            }
+        });
+
     }
 }
